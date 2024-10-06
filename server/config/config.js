db300548cfe4c29b760fd65ea825d5da300603e6
akshat <mejatin03gmail.com>
2024-10-06 17:15:48 +0530

    pg

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
+const pg = require('pg');
 const { Sequelize } = require('sequelize');
-import pg from 'pg';
+
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
