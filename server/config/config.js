ebe3dff6f6ff32c04c5e62c8db456421225d4233
akshat <mejatin03gmail.com>
2024-10-06 17:08:54 +0530

    pg

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,12 @@
 const { Sequelize } = require('sequelize');
+import pg from 'pg';
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false, // To disable SQL logging, helps during development
+    dialectModule: pg
 });
 
 module.exports = sequelize;
